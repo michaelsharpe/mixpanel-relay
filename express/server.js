59bef60bd65e74d031b48213bf9942f10bcbfe66
Michael Sharpe <m.sharpe@project89.org>
2020-07-07 16:14:15 -0700

    Add in rrrelay support for basic mixpanel data

--- express/server.js
+++ express/server.js
@@ -1,22 +1,103 @@
 'use strict';
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path');
 const serverless = require('serverless-http');
-const app = express();
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+app.use(morgan('combined'))
 
 const router = express.Router();
-router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+
+router.get('/:anonymousId', (req, res) => {
+  console.log("getting mixpanel data");
+  const mixpanelApiUrl = 'https://mixpanel.com/api/2.0/jql';
+  const mixpanelApiSecret = process.env.MIXPANEL_SECRET;
+
+  const toDate = new Date(Date.now());
+  const fromDate = new Date(toDate);
+  // calculate 30 days ago:
+  fromDate.setDate(toDate.getDate() - 5);
+
+  const toDateISO = toDate.toISOString().slice(0,10);
+  const fromDateISO = fromDate.toISOString().slice(0,10);
+
+  const {anonymousId} = req.params;
+
+  // Your JQL here:
+  const params = {
+    script: `
+  const FROM_DATE = '${fromDateISO}';
+  const TO_DATE = '${toDateISO}';
+  function main() {
+    return join(
+      Events({
+        from_date: FROM_DATE,
+        to_date:   TO_DATE,
+        event_selectors:  [{
+          event: 'Loaded a Page'
+        }]
+      }),
+      People(),
+      {
+        selectors:[{selector:
+          'properties["mpAnonymousId"] == "${anonymousId}"'}]
+      }
+    )
+    .sortDesc('time')
+  }
+  `
+  };
+
+  const basicAuth = Buffer.from(mixpanelApiSecret).toString('base64');
+
+  const formData = Object.keys(params).map((key) => {
+    return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+  }).join('&');
+
+  fetch(mixpanelApiUrl, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded; charset=utf-8',
+      'Authorization': 'Basic ' + basicAuth,
+      'Accept': 'application/json',
+    },
+    body: formData
+  })
+    .then(response => {
+      if(response.ok) {
+        return response.json();
+      } else {
+        console.log(response);
+        throw "Bad Response";
+      }
+    })
+    .then(json => {
+      console.log(json);
+      return json;
+    })
+    .then(json => json.map(data => ({
+      websiteId: data.event.properties.websiteId,
+      time: data.event.time
+    })))
+    .then(data => data.sort((a, b) => a.time < b.time ? -1 : 1))
+    .then(data => res.json(data)) 
+    .catch(err => {
+      console.log(err);
+
+      res.status(400).send(err);
+    });
 });
-router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
-router.post('/', (req, res) => res.json({ postBody: req.body }));
 
 app.use(bodyParser.json());
-app.use('/.netlify/functions/server', router);  // path must route to lambda
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
+app.use(router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
